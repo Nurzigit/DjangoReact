e4685a64ba135f992cbd7217678ff68baf82f65b
Nurzhigit <nurzigitturman@gmail.com>
2023-04-13 11:17:29 +0300

    delete file, and install new library

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <div className={'footer'}>
+            <h1>Hello footer</h1>
+        </div>
+    );
+};
+
---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className={'Header'}>
+            <h1>Hello header</h1>
+        </div>
+    );
+};
+
